d2b9b3ae81fe93dce94bceb057e811b65d8bd5d6
thhuynh7 <thhuynh7@myseneca.ca>
2022-05-26 19:31:28 -0400

    set up Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
+// .eslintrc.js
+
 module.exports = {
   env: {
+    node: true,
     commonjs: true,
     es2021: true,
-    node: true,
+    // Add this next line to configure ESLint for Jest, see:
+    // https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+    jest: true,
   },
   extends: 'eslint:recommended',
   parserOptions: {
