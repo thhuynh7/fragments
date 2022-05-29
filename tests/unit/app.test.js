8c40bc851931bd27deeffccccfd00630c519d956
thhuynh7 <thhuynh7@myseneca.ca>
2022-05-28 21:23:18 -0400

    mod app.test.js

--- tests/unit/app.test.js
+++ tests/unit/app.test.js
@@ -1,11 +1,10 @@
 // tests/unit/app.test.js
 
 const request = require('supertest');
-
 const app = require('../../src/app');
-describe('GET /v2', () => {
+describe('GET /v99', () => {
   // If the request is missing the Authorization header, it should be forbidden
-  test('404 not found', () => request(app).get('/v2').expect(404));
+  test('404 not found', () => request(app).get('/v99').expect(404));
 });
 
 
