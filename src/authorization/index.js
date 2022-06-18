3cfd13138fbcccb75d4b56664b7c317aee74168d
thhuynh7 <thhuynh7@myseneca.ca>
2022-06-18 18:38:17 -0400

    add Dockerfile

--- src/authorization/index.js
+++ src/authorization/index.js
@@ -1,14 +1,29 @@
 // src/authorization/index.js
 // prefer .htpasswd file when running in Development mode locally (i.e., npm run start:prod)
-if (process.env.HTPASSWD_FILE && process.env.NODE_ENV !== 'production') {
-  module.exports = require('./basic-auth');
-}
+// if (process.env.HTPASSWD_FILE && process.env.NODE_ENV !== 'production') {
+//   module.exports = require('./basic-auth');
+// }
 // use Amazon Cognito in Production mode or running on AWS EC2 instance
-else if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
+// else if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
+//   module.exports = require('./cognito');
+// }
+
+// In all other cases, we need to stop now and fix our config
+// else {
+//   throw new Error('missing env vars: no authorization configuration found');
+// }
+// src/authorization/index.js
+
+// Prefer Amazon Cognito
+if (process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID) {
   module.exports = require('./cognito');
 }
-
+// Also allow for an .htpasswd file to be used, but not in production
+else if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
+  module.exports = require('./basic-auth');
+}
 // In all other cases, we need to stop now and fix our config
 else {
   throw new Error('missing env vars: no authorization configuration found');
 }
+
